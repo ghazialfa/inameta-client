b8b890e136a4a663eda1844087d414aa10917c0c
ghazialfa <ghazialfawwaz01@gmail.com>
2025-11-27 16:15:19 +0700

    basepath

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,7 @@ import type { NextConfig } from "next"
 const nextConfig: NextConfig = {
     /* config options here */
     // devIndicators: false
+    basePath: '/lite',
     assetPrefix: '/lite',
     async rewrites() {
         return [
